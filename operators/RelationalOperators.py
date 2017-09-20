bc0ef1b7472e4c613771231c14b63c056e4e7624
Shankar Pilli <shankar.pilli@sparity.com>
2017-09-20 16:31:24 +0530

    Added Relational Operators

---
+++ operators/RelationalOperators.py
@@ -0,0 +1,21 @@
+#This is the example for the relational or comparision operators
+
+a = input ("Enter your number a : ")
+b = input ("Enter your number b : ")
+
+print "== ralational operator : "+str(a==b)
+print "> ralational operator : "+str(a>b)
+print "< ralational operator : "+str(a<b)
+print "!= ralational operator : "+str(a!=b)
+print ">= ralational operator : "+str(a>=b)
+print "<= ralational operator : "+str(a<=b)
+
+#Output
+'''Enter your number a : 1
+Enter your number b : 1
+== ralational operator : True
+> ralational operator : False
+< ralational operator : False
+!= ralational operator : False
+>= ralational operator : True
+<= ralational operator : True'''
