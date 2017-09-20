72e013595fff10ead1f6c9bce7b8dca87926902e
Shankar Pilli <shankar.pilli@sparity.com>
2017-09-20 15:54:56 +0530

    Added logical operators

---
+++ operators/LogicalOperators.py
@@ -0,0 +1,20 @@
+# This is the example of logical operators
+
+a = input("Enter your number a : ")
+b = input('Enter your number b : ')
+
+#Logical and
+print a<b and a>b
+
+#Logical or
+print a<b or a>b
+
+#Logical not
+print not a<b
+
+#OutPut
+'''Enter your number a : 20
+Enter your number b : 12
+False
+True
+True'''
