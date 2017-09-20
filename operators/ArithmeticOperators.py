2ed3d5d6520ee937297b812c8be15ddb841f4a34
Shankar Pilli <shankar.pilli@sparity.com>
2017-09-20 16:23:31 +0530

    Added arithmetic operators

---
+++ operators/ArithmeticOperators.py
@@ -0,0 +1,36 @@
+#Python Arithmetic Operators
+
+number  = input("Enter your first number : ")
+number_2 = input ("Enter your last number : ")
+
+#Addition
+print ("Addition + : "+str(number+number_2))
+
+#Subtraction
+print ("Subtraction - : "+str(number-number_2))
+
+#Multiply
+print ("Multiplication * : "+str(number*number_2))
+
+#Division
+print ("Division / : "+str(number*number_2))
+
+#Moduls
+print ("Modulus % : "+str(number%number_2))
+
+#Exponent
+print ("Exponent ** : "+str(number**number_2))
+
+#Floor Division
+print ("Floor Division // : "+str(number//number_2))
+
+#Output
+'''Enter your first number : 12
+Enter your last number : 2
+Addition + : 14
+Subtraction - : 10
+Multiplication * : 24
+Division / : 24
+Modulus % : 0
+Exponent ** : 144
+Floor Division // : 6'''
