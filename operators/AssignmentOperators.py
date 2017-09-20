7cdd671cf831b1e537c870d5b621c64b7c7b2a9d
Shankar Pilli <shankar.pilli@sparity.com>
2017-09-20 16:12:03 +0530

    Added Assignment Operators

---
+++ operators/AssignmentOperators.py
@@ -0,0 +1,34 @@
+#The is the Example for Assigment operators
+a = input ("Enter your number a : ")
+
+# "=" Assignment the input will store in the a
+# Add AND --> a = a+10
+a += 10
+print "Add AND : "+str(a)
+# Subtraction AND --> a = a-10
+a -= 10
+print "Subtraction AND : "+str(a)
+#Multiply AND --> a = a*10
+a *= 10
+print "Multiply AND : "+str(a)
+#Division AND --> a = a/10
+a /= 10
+print "Division AND : "+str(a)
+#Moduls AND --> a = a%10
+a %=10
+print "Moduls AND : "+str(a)
+#Exponent AND  --> a = a**10
+a **=10
+print "Exponent AND : "+str(a)
+#Floor Division AND --> a = a//10
+a //=10
+print "Floor Division AND : "+str(a)
+
+'''Enter your number a : 10
+Add AND : 20
+Subtraction AND : 10
+Multiply AND : 100
+Division AND : 10
+Moduls AND : 0
+Exponent AND : 0
+Floor Division AND : 0'''
