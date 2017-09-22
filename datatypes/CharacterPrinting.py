8c5ac57ef24fbfbc46d4eace49bab6fedd8e3d0b
sixscorpions <sandy0210>
2017-09-22 18:10:47 +0530

    Added other string funcations

---
+++ datatypes/CharacterPrinting.py
@@ -0,0 +1,17 @@
+#Character Printing
+
+name = str(raw_input("Enter your name : "))
+
+print "name[0] : "+name[0]
+print "name[1:3] : "+name[1:3]
+print "name[:3] : "+name[:3]
+print "name[1:] : "+name[1:]
+print "name[:] : "+name[:]
+
+#output
+'''Enter your name : shankar
+name[0] : s
+name[1:3] : ha
+name[:3] : sha
+name[1:] : hankar
+name[:] : shankar'''
---
+++ datatypes/CountFind.py
@@ -0,0 +1,13 @@
+#Count
+
+name = str(raw_input("Enter your name : "))
+
+print "Str Count : "+str(name.count("ab"))
+
+#Find
+print "Str Find : "+str(name.find("shankar"))
+
+#Output
+'''Enter your name : sa shankar asfjka ab akkjskfa ab
+Str Count : 2
+Str Find : 3'''
