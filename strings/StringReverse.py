460c710ed83a9d7f450c582a3738d5f5a858d53a
Shankar Pilli <shankar.pilli@sparity.com>
2017-09-25 16:01:58 +0530

    added string reverse

--- strings/StringReverse.py
+++ strings/StringReverse.py
@@ -1,1 +1,8 @@
 #This this the program for the reverse the string
+
+getString = str(raw_input("Enter your string : "))
+print "Reversed string : "+getString[::-1]
+
+#Output:
+Enter your string : hello
+Reversed string : olleh
